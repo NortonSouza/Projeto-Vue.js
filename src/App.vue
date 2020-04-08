e8ff0d9a902ffbdf2b941955f9ffbc9e6352fe26
Norton <norton.souza@gmail.com>
2020-04-07 23:34:56 -0300

    Criação de conversor de moedas com Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1 class="tituloCentro">Conversor de Moedas</h1>
+        <div class="linha">
+            <Conversor moeda-a="USD" moeda-b="BRL" />
+            <Conversor moeda-a="BRL" moeda-b="USD" />
+        </div>
+        <div class="linha">
+            <Conversor moeda-a="CAD" moeda-b="BRL" />
+            <Conversor moeda-a="BRL" moeda-b="CAD" />
+        </div>
+        <div class="linha">
+            <Conversor moeda-a="EUR" moeda-b="BRL" />
+            <Conversor moeda-a="BRL" moeda-b="EUR" />
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import Conversor from "./components/Conversor.vue";
+
+
+    export default {
+        name: 'App',
+        components: {
+            Conversor
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        /*text-align: center;*/
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    .linha{
+        margin-top:40px;
+        display:flex;
+        justify-content:space-around;
+    }
+
+    .tituloCentro{
+        text-align:center;
+    }
 </style>
